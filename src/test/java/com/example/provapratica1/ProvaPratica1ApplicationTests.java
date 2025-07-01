ef912e27e84b93133ef76e94257edfbf9b8f4d79
matheus_estacio <matheusdiasestacioo@gmail.com>
2025-07-01 09:55:21 -0300

    first commit

---
+++ src/test/java/com/example/provapratica1/ProvaPratica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provapratica1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaPratica1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
